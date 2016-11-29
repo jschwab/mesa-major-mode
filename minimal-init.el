26adb5f7cb3f2614df0f964e6f22cf199a1f7660
Josiah Schwab <jschwab@gmail.com>
2016-11-29 13:33:19 -0800

    Update minimal init

--- minimal-init.el
+++ minimal-init.el
@@ -5,10 +5,12 @@
 ;;   the folder where you placed the mesa-mode.el file
 (add-to-list 'load-path "/path/to/mesa-major-mode/")
 (require 'mesa-mode)
+(require 'run-star-extras)
 
 ;; set default MESA version
-(setq mesa-default-version "r7624")
+(setq mesa-default-version "r8845")
 
 ;; set default file associations
 (add-to-list 'auto-mode-alist '("/inlist[^/]*$" . mesa-mode))
-(add-to-list 'auto-mode-alist '("\\.defaults\\'" . (lambda () (mesa-mode) (view-mode))))
+(add-to-list 'auto-mode-alist '("\\.defaults$" . (lambda () (mesa-mode) (view-mode))))
+(add-to-list 'auto-mode-alist '("/run_star_extras.f$" . (lambda () (f90-mode) (run-star-extras-minor-mode))))
